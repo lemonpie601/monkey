5e787279c0265dd935628b9b95b29f4a48201579
lemonpie601 <valorsida@gmail.com>
2026-05-30 21:28:10 +0900

    Create uniscenepainter.user.js

---
+++ uniscenepainter.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Univers Scene Painter
+// @namespace    univers-scene-painter
+// @version      3.8.4
+// @description  Univers  Scene Painter 외부 코어 로더
+// @match        https://www.univers.chat/*
+// @grant        GM_xmlhttpRequest
+// @connect      generativelanguage.googleapis.com
+// @connect      api.openai.com
+// @connect      api.anthropic.com
+// @connect      open.bigmodel.cn
+// @connect      openrouter.ai
+// @connect      aiplatform.googleapis.com
+// @connect      *.aiplatform.googleapis.com
+// @connect      image.novelai.net
+// @connect      api.novelai.net
+// @connect      novelai.net
+// @connect      *.novelai.net
+// @connect      *
+// @require      https://cdn.jsdelivr.net/npm/fflate@0.8.2/umd/index.js
+// @require      https://gist.github.com/lemonpie601/6a7aa77944950bb6acad6ab71fb93a45/raw/580c5b36f76a6e7e7f451c523c798dc6df8c4885/uniscenepainter-core.user.js
+// @updateURL    https://gist.github.com/lemonpie601/6a7aa77944950bb6acad6ab71fb93a45/raw/580c5b36f76a6e7e7f451c523c798dc6df8c4885/uniscenepainter.user.js
+// @downloadURL  https://gist.github.com/lemonpie601/6a7aa77944950bb6acad6ab71fb93a45/raw/580c5b36f76a6e7e7f451c523c798dc6df8c4885/uniscenepainter.user.js
+// @run-at       document-idle
+// ==/UserScript==
